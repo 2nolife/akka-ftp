87acdd950bfdb62e968c7fb6cca9562b13960fef
sector <sector@coldcore.com>
2015-12-18 17:07:53 +0000

    Utility

---
+++ src/main/resources/web/js/utils.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function bytesToSize(bytes) {
+  var sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+  if (bytes == 0) return '0 B';
+  var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+}
